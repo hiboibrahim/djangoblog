48cea8b6838d34313921f3d83a3efd89bc603296
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-14 15:35:46 +0000

    add generic list view with filters

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['8000-hiboibrahim-djangoblog-319epf9cnon.ws-eu106.gitpod.io',
 '.herokuapp.com'
