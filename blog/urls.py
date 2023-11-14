53fb616a5a6687fd9f05d85813096413316dde08
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-14 16:15:18 +0000

    added summernote package

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path('posts/', views.PostList.as_view(), name='home'),
 ]
--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
+    
+    
+    
 ]
