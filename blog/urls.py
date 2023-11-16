fd22e7bc5d72f7aa0227e40a9f3f8df431f8dd11
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-16 21:44:10 +0000

    update views and urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
 from . import views
+from django.urls import path
+
 
 
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path("<int:event_id>/", views.event_detail, name="event_detail")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Post
+from .models import Event 
 
 
 # Create your views here.
+
+class EventsList(generic.ListView):
+    model = Event
+    template_name = "index.html"
+    paginate_by = 12
+
+
+
+def event_detail(request, event_id):
+    
+    queryset = Event.objects.all()
+    event = get_object_or_404(Event, event_id=event_id)
+
+    return render(
+        request,
+        "events/event_detail.html",
+        {"event": event}
+    )
+
+
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
@@ -11,19 +31,7 @@ class PostList(generic.ListView):
     paginate_by = 6
 
 def post_detail(request, slug):
-    """
-    Display an individual :model:`blog.Post`.
-
-    **Context**
-
-    ``post``
-        An instance of :model:`blog.Post`.
-
-    **Template:**
-
-    :template:`blog/post_detail.html`
-    """
-
+    
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post},)
+    return render(request, "blog/post_detail.html", {"post": post, "coder": "Hibo Ibrahim"},)
