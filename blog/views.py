3fa208dd26210c7425ce7a119ec7eda6e540754e
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-19 08:07:16 +0000

    add collaborate model and view

--- blog/views.py
+++ blog/views.py
@@ -21,8 +21,6 @@ def post_detail(request, slug):
     comment_count = post.comments.filter(approved=True).count()
     
     if request.method == "POST":
-                print("Received a POST request")
-
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -33,9 +31,7 @@ def post_detail(request, slug):
                 request, messages.SUCCESS,
                 'Comment submitted and awaiting approval'
             )
-    
     comment_form = CommentForm()
-        print("About to render template")
 
 
     return render(
