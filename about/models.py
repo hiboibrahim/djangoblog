8e0929e57d03b0d5a821b0bafa9c52b873fbe96b
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-20 11:42:12 +0000

    add cloudinary

--- about/models.py
+++ about/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from Cloudinary.models import CloudinaryField
 
 # Create your models here.
 
 class About(models.Model):
     title = models.CharField(max_length=100, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
     author = models.CharField()
+
 
     def __str__(self):
         return self.title
